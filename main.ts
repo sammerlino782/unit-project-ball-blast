5e5e64472c998d3cdce6f9a1587864328ffe716b
ZDuckq <michael.strimbu@student.csd509j.net>
2026-02-17 17:18:09 +0000

    Fixed Issues

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
+
+game.onUpdateInterval(100, function() {
+    player.ay += 3
+})
+
 function startGame () {
-    let player = sprites.create(assets.image`playericon`, SpriteKind.Player)
-    controller.moveSprite(player)
-
+    scene.setBackgroundColor(9)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    tiles.placeOnTile(player, tiles.getTileLocation(6, 13))
+    scene.cameraFollowSprite(player)
+    controller.moveSprite(player, 100, 0)
 } 
-
+let player = sprites.create(assets.image`Tem`, SpriteKind.Player)
 startGame()
