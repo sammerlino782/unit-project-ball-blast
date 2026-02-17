e4da484fb38b5efcb6a8c0e386b10f8d03cf15b2
sammerlino782 <sam.merlino@student.csd509j.net>
2026-02-17 13:05:09 -0800

    Update pxt.json, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
-
 function startGame () {
     scene.setBackgroundColor(9)
+    let player = sprites.create(assets.image`playerimage`, SpriteKind.Player)
     tiles.setCurrentTilemap(tilemap`level1`)
-    tiles.placeOnTile(player, tiles.getTileLocation(6, 13))
-    scene.cameraFollowSprite(player)
-    controller.moveSprite(player, 100, 0)
+    controller.moveSprite(player)
 }
-let player = sprites.create(assets.image`playerimage`, SpriteKind.Player)
+
 startGame()
