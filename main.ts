94e3aff7aa30ab87cb320d86d1e5bd1fd7152e77
sammerlino782 <sam.merlino@student.csd509j.net>
2026-02-17 12:59:47 -0800

    Update main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
 
-game.onUpdateInterval(100, function() {
-    player.ay += 3
-})
-
 function startGame () {
     scene.setBackgroundColor(9)
     tiles.setCurrentTilemap(tilemap`level1`)
     tiles.placeOnTile(player, tiles.getTileLocation(6, 13))
     scene.cameraFollowSprite(player)
     controller.moveSprite(player, 100, 0)
-} 
-let player = sprites.create(assets.image`Tem`, SpriteKind.Player)
+}
+let player = sprites.create(assets.image`playerimage`, SpriteKind.Player)
 startGame()
