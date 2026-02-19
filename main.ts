69aefebedc3c99c7726196ece1dd8ca88b43a7d5
sammerlino782 <sam.merlino@student.csd509j.net>
2026-02-19 08:35:34 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,3 +6,28 @@ function startGame () {
 }
 
 startGame()
+
+game.onUpdateInterval(1500, function () {
+    if (controller.A.isPressed()) {
+    let enemy = sprites.create(img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . 2 2 2 2 2 2 2 2 2 2 . .
+        . . . . 2 2 2 2 2 2 2 2 2 2 . .
+        . . . . 2 2 2 2 2 2 2 2 2 2 . .
+        . . . . 2 2 2 2 2 2 2 2 2 2 . .
+        . . . . 2 2 2 2 2 2 2 2 2 2 . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `, SpriteKind.Enemy)
+    enemy.setVelocity(randint(-40, -50), 0)
+    enemy.setPosition(randint(130, 150), randint(10, screen.height - 10))
+    }
+})
